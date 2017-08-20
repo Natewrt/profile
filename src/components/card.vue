8cf19c090aa4b6643cb2287fd428c69bc0f4ba83
natewrt <nathamon.nt109@gmail.com>
2017-08-20 21:00:43 +0700

    finish the rest problem with imgs declaration

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,8 +1,11 @@
 <template>
     <section class="container">
         <div id="card">
-            <figure class="front"><br><br>Mynd</figure>
-            <figure class="back"><br>The<br><br>marketing</figure>
+            <figure class="front"><font size="7"><br><br><br>Mynd</font></figure>
+            <figure class="back"><br>She is Kitty<br>STAY AWAY human!<br>
+              <br><img src="https://media.giphy.com/media/TaBJhZTUALPlm/giphy.gif" alt="" width="300" height="300">
+            </figure>
+                
         </div>
     </section>
 </template>
@@ -13,11 +16,12 @@
 <style scoped>
     .container {
         margin: 5% 0 0 40%;
-        width: 150px;
-        height: 200px;
+        width: 350px;
+        height: 400px;
         position: relative;
         perspective: 300px;
     }
+    
     #card {
         width: 100%;
         height: 100%;
@@ -35,7 +39,7 @@
         color: white;
         text-align: center;
         font-weight: bold;
-        font-size: 30px;
+        font-size: 20px;
         backface-visibility: hidden;
     }
     #card .front {
